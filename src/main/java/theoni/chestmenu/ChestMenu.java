36c781fa212bd15521a7d399c7fadf0ac29c03b6
Arkadiy <mefrreex@gmail.com>
2023-02-04 23:44:47 +0200

    Added bStats

--- src/main/java/theoni/chestmenu/ChestMenu.java
+++ src/main/java/theoni/chestmenu/ChestMenu.java
@@ -1,10 +1,12 @@
 package theoni.chestmenu;
 
+import cn.nukkit.Server;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
 import theoni.chestmenu.commands.Commands;
 import theoni.chestmenu.commands.OpenCommand;
 import theoni.chestmenu.inventories.Inventories;
+import theoni.chestmenu.metrics.Metrics;
 import lombok.Getter;
 
 public class ChestMenu extends PluginBase {
@@ -27,5 +29,12 @@ public class ChestMenu extends PluginBase {
 
         this.getServer().getCommandMap().register("ChestMenu", new OpenCommand(this));
         new Commands().register(this, menu);
+    }
+
+    public void metrics() {
+        int pluginId = 17642;
+        Metrics metrics = new Metrics(this, pluginId);
+        metrics.addCustomChart(new Metrics.SimplePie("server_movement", () -> String.valueOf(getConfig().getBoolean("PowerNukkiX-movement-server"))));
+        metrics.addCustomChart(new Metrics.SimplePie("nukkit_version", () -> Server.getInstance().getNukkitVersion()));
     }
 }
